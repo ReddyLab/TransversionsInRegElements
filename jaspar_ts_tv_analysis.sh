bc783a5c69d60b0256a7884454eaa42c4d4c3ef8
Tim Reddy <tim.reddy@duke.edu>
2015-11-16 10:51:44 -0500

    Added status updates to jaspar analysis
    Included status messages for the Jaspar analysis script

--- jaspar_ts_tv_analysis.sh
+++ jaspar_ts_tv_analysis.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Beginning jaspar Ts:Tv analysis"
 python Effects_on_TFBS_motif_scores.py > jaspar_Ts_Tv.tab
+echo "Calculated predicted effects of all possible mutations"
+echo "Beginning regression analysis"
 Rscript jaspar_regression_analysis.R
